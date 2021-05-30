81396b4bad508f5fb51431ab81cc68592c529683
merveealpay <merveealpay@gmail.com>
2021-05-30 22:06:31 +0300

    extracting href links

--- crawler.py
+++ crawler.py
@@ -3,8 +3,9 @@ import requests
 
 def request(url):
     try:
+        #requests.request("GET")
         return requests.get("http://" + url)
-    except requests.exceptions.ConnectionError:
+    except requests.exceptions.HTTPError:
         pass
 
 target_url = "google.com"
@@ -12,7 +13,7 @@ target_url = "google.com"
 with open("subdomain-wordlist.txt", "r") as wordlist:
     for line in wordlist:
         stripped_line = line.strip()
-        test_url = stripped_line + "." + target_url
-        resp = request(test_url)
-        if resp:
+        test_url = f"{stripped_line}.{target_url}"
+        #test_url = stripped_line + "." + target_url
+        if request(test_url):
             print("[+] discovered subdomain ---> " + test_url)
